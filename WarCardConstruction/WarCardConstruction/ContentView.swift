17323165ca047af340c7c6dd1f6000c8d7e40826
Shruthi012 <110873825+Shruthi012@users.noreply.github.com>
2022-08-11 13:35:33 -0500

    Add files via upload

---
+++ WarCardConstruction/WarCardConstruction/ContentView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  WarCardConstruction
+//
+//  Created by Suren R on 8/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    var body: some View {
+        ZStack {
+            
+            
+            Image("background").ignoresSafeArea()
+    
+            
+            VStack {
+            Spacer()
+                
+            Image("logo")
+            Spacer()
+                
+            HStack{
+            Spacer()
+                    
+            Image(playerCard)
+            Spacer()
+            Image(cpuCard)
+            Spacer()
+            }
+            Spacer()
+                Button(action: {
+                    
+            let  playerRand = Int.random(in: 2...14)
+            let  cpuRand = Int.random(in: 2...14)
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    if playerRand > cpuRand {
+                    playerScore += 1
+                    }
+                    else if cpuRand > playerRand {
+                    cpuScore += 1
+                    }
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+            
+                
+            Spacer()
+            HStack{
+            Spacer()
+            VStack{
+                Text("Player")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+                    .padding(.bottom, 5.0)
+                Text(String(playerScore))
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+            }
+            Spacer()
+            VStack{
+                Text("CPU")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+                    .padding(.bottom, 5.0)
+                Text(String(cpuScore))
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+            }
+             Spacer()
+            }
+            Spacer()
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WarCardConstruction/WarCardConstruction/WarCardConstructionApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardConstructionApp.swift
+//  WarCardConstruction
+//
+//  Created by Suren R on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WarCardConstructionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
